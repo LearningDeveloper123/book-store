f9c63443496dfc01b540e52daee2f9e07d72cc9b
LearningDeveloper123 <niatwebinar2@gmail.com>
2025-07-29 18:54:14 +0530

    route atatic and dynamic basic application

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter,Routes,Route,Navigate } from "react-router";
+
+import Home from "./components/Home";
+import BookList from "./components/BookList";
+import BookDetails from "./components/BookDetails";
+import Checkout from "./components/Checkout";
+import Cart from "./components/Cart";
+import NotFound from "./components/NotFound";
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+<BrowserRouter>
+<Routes>
+  <Route path ="/" element = {<Home/>}/>
+  <Route path ="/books" element = {<BookList/>}/>
+  <Route path ="/books/:id" element = {<BookDetails/>}/>
+  <Route path ="/checkout" element = {<Checkout/>}/>
+  <Route path ="/cart" element = {<Cart/>}/>
+  <Route path = "*" element = {<Navigate to ="/not-found"/>}/>
+</Routes>
+</BrowserRouter>   
   )
 }
 
